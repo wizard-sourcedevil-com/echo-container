cd0c044db09b353d2797b73fa7ef31008b732a73
sourcedevil <wizard@sourcedevil.com>
2025-01-22 14:27:41 -0600

    now supporting printing all request information

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Variable to store the last request details
+LAST_REQUEST="No requests received yet."
+
+# Function to handle HTTP requests
+handle_request() {
+  REQUEST_DETAILS="Path: $REQUEST_URI\nHeaders:\n$(env)"
+  echo -e "HTTP/1.1 200 OK\n\nHello World\n\n$REQUEST_DETAILS"
+  echo -e "Request received:\n$REQUEST_DETAILS" >&2
+  LAST_REQUEST=$REQUEST_DETAILS
+}
+
 # Start the HTTP server in the background
-socat TCP-LISTEN:8080,reuseaddr,fork EXEC:'/bin/echo -e HTTP/1.1 200 OK\\n\\nHello World' &
+socat TCP-LISTEN:8080,reuseaddr,fork SYSTEM:'handle_request' &
 
 # Read the environment variable
 # infinite loop the ECHO_NAMESPACE environment variable
 while true; do
   echo "ECHO_NAMESPACE AND HTTP: $ECHO_NAMESPACE"
-  sleep 5
+  echo -e "Last request details:\n$LAST_REQUEST"
+  sleep 25
 done
