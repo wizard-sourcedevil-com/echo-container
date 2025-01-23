86896f8f04e07a11a8f87857e67efdf39aaeb918
sourcedevil <wizard@sourcedevil.com>
2025-01-22 19:19:43 -0600

    fixing the container

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,8 @@
 # Variable to store the last request details
 LAST_REQUEST="No requests received yet."
 
-# Function to handle HTTP requests
-handle_request() {
-  REQUEST_DETAILS="Path: $REQUEST_URI\nHeaders:\n$(env)"
-  echo -e "HTTP/1.1 200 OK\n\nHello World\n\n$REQUEST_DETAILS"
-  echo -e "Request received:\n$REQUEST_DETAILS" >&2
-  LAST_REQUEST=$REQUEST_DETAILS
-}
-
 # Start the HTTP server in the background
-socat TCP-LISTEN:8080,reuseaddr,fork SYSTEM:'handle_request' &
+socat TCP-LISTEN:8080,reuseaddr,fork SYSTEM:'REQUEST_DETAILS="Path: $REQUEST_URI\nHeaders:\n$(env)"; echo -e "HTTP/1.1 200 OK\n\nHello World\n\n$REQUEST_DETAILS"; echo -e "Request received:\n$REQUEST_DETAILS" >&2; LAST_REQUEST=$REQUEST_DETAILS' &
 
 # Read the environment variable
 # infinite loop the ECHO_NAMESPACE environment variable
